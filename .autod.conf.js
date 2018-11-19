0c9fc4027dd858ab43dea6fa1274cc372158d942
popomore <sakura9515@gmail.com>
2018-11-19 17:28:24 +0800

    f

--- .autod.conf.js
+++ .autod.conf.js
@@ -21,7 +21,8 @@ module.exports = {
     'egg-bin@1',
     'inquirer@3',
     'eslint@4',
-    'eslint-config-egg@6'
+    'eslint-config-egg@6',
+    'mem-fs-editor@4',
   ],
   registry: 'https://r.cnpmjs.org',
 };
